d344b8f7a411cb643cfffaf97119a6bd56697974
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-05-17 12:26:38 -0700

    compleded

--- interviewOCweekly/week1_basic_recursion_dp/NonConsecutiveOnes.py
+++ interviewOCweekly/week1_basic_recursion_dp/NonConsecutiveOnes.py
@@ -20,3 +20,44 @@
 
 # The order of the strings in the array does not matter.
 # Use recursion to solve this problem. 
+
+
+# in - int n
+# out - [str]
+
+def noConsecOnes(n):
+    def helper(sub):
+        if len(sub)>= n:
+            return set([sub])
+        results = helper(sub+"0")
+        if len(sub) == 0 or sub[len(sub)-1]!="1":
+            results = results.union(helper(sub + "1"))
+        return results
+    return list(helper(""))
+print(noConsecOnes(2))
+
+
+def noConsecOnes2(n):
+    def helper(sub):
+        if len(sub)>= n:
+            return set([sub])
+        results0 = helper(sub+"0")
+        if len(sub) == 0 or sub[len(sub)-1]!="1":
+            results1 = helper(sub + "1")
+        else:
+            results1 = set([])
+        results = results0.union(results1)
+        
+        return results
+    return list(helper(""))
+print(noConsecOnes2(2))
+
+# 2
+# 00 01 10 (x11)
+
+# 3
+# 000 001 010 100 101 (x111)   
+
+# takeaway:
+#     input and output are different and assign the result of variable to not a input sub but to results
+#     the second pattern set needs to be add together with .union 
