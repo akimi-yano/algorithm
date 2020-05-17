25d6e1ab90d89e62cd613eb72f2212f9d08ee47f
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-05-17 10:58:46 -0700

    O(N) solution

--- interviewOCweekly/week1_basic_recursion_dp/TwoSum.py
+++ interviewOCweekly/week1_basic_recursion_dp/TwoSum.py
@@ -1,6 +1,7 @@
 # 203 - Two Sum
 
-# Given an array of integers and a target, return a pair of indices where the corresponding values in the array add up to the target.
+# Given an array of integers and a target, 
+# return a pair of indices where the corresponding values in the array add up to the target.
 
 # ```
 # Input: Array of Integers, Target Integer
@@ -20,3 +21,19 @@
 # If the target integer is not found return [-1, -1].
 
 # Values of the array can be positive or negative integers.
+
+def twoSum(arr,target):
+    checker = {}
+    ans = []
+    for i in range(len(arr)):
+        if arr[i] not in checker:
+            checker[target-arr[i]] = i 
+        else:
+            ans.append(checker[arr[i]])
+            ans.append(i)
+            break
+    if len(ans)<1:
+        ans = [-1,-1]
+    return ans
+print(twoSum([1, 6, -5, 7, 3],-2))
+print(twoSum([1, 9, 10], 8))
