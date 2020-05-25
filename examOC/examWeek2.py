d1612d16bf4d033202998a9b33e7770e10a988c7
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-05-24 17:35:00 -0700

    zutaboro

--- examOC/examWeek2.py
+++ examOC/examWeek2.py
@@ -75,7 +75,8 @@ def condense(head):
 
 # Problem 2. Check the Structure
 
-# A binary tree uses a multi-node data structure where each node may have 0 to 2 child nodes, and has one stored value, its node number in this case. A tree may either be:
+# A binary tree uses a multi-node data structure where each node may have 0 to 2 child nodes, 
+# and has one stored value, its node number in this case. A tree may either be:
 
 # An empty tree, the root is null.
 # A non-empty tree with a non-null root node that contains a value and up to 2 subtrees, left and right, which are also binary trees.
@@ -87,7 +88,8 @@ def condense(head):
 # The right subtree of each node contains only nodes with values that are higher than its own value.
 
 
-# A pre-order traversal is a recursive tree traversal method where the current node is visited first, then the left subtree, and then the right subtree. The following pseudocode parses a tree into a list using pre-order traversal:
+# A pre-order traversal is a recursive tree traversal method where the current node is visited first, 
+# then the left subtree, and then the right subtree. The following pseudocode parses a tree into a list using pre-order traversal:
 
 # If the root is null, return the null list.
 # For a non-null root node:
@@ -96,7 +98,9 @@ def condense(head):
 # Return the concatenated list: the non-null node + left + right.
 
 
-# Determine whether a traversal history can describe a path for a valid BST. Given a traversal history of one or many binary trees, check whether each path represents a valid BST or not. Return an array of strings, one for each test. Answer YES if the path can represent a valid BST, or NO if it cannot.
+# Determine whether a traversal history can describe a path for a valid BST. 
+# Given a traversal history of one or many binary trees, check whether each path represents a valid BST or not. 
+# Return a string, one for each test. Answer YES if the path can represent a valid BST, or NO if it cannot.
 
 # Example
 
@@ -108,7 +112,7 @@ def condense(head):
 # Next 1 is less than 2. Place the node 1 at the left of node 2.
 # Next 3 is greater than 2. Place the node 3 at the right of node 2.
 # Next 4 is greater than 3. Place the node 4 at the right of node 3.
-# Next 5 is greater than 5. Place the node 5 at the right of node 4.
+# Next 5 is greater than 4. Place the node 5 at the right of node 4.
 # This graph meets the definition of a BST.
 
 # Function Description 
@@ -141,30 +145,30 @@ def condense(head):
 
 # STDIN          Function
 # -----          --------
-# 5              →   number of queries q = 5
+
 # 3              →   a[] size n = 3 (query 0)
 # 1 3 2          →   a = [1, 3, 2]
+
 # 3              →   a[] size n = 3 (query 1)
 # 2 1 3          →   a = [2, 1, 3]
+
 # 6              →   a[] size n = 6 (query 2)
 # 3 2 1 5 4 6    →   a = [3, 2, 1, 5, 4, 6]
+
 # 4              →   a[] size n = 4 (query 3)
 # 1 3 4 2        →   a = [1, 3, 4, 2]
+
 # 5              →   a[] size n = 5 (query 4)
 # 3 4 5 1 2      →   a = [3, 4, 5, 1, 2]
-# Sample Output 0
 
+# output: 
 # YES
 # YES
 # YES
 # NO
 # NO
-# Explanation 0
-
-# The diagrams below show BST representations with green nodes at valid locations and red at invalid.
 
 
-# An explanation of the q = 5 queries:
 
 # Diagram (a) is valid, so return the string YES.
 # Diagram (b) is valid, so return the string YES.
