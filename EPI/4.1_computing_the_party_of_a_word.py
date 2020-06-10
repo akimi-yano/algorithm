ef77fc0a3ca047aac1798cddf0d6637f455c7b21
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-06-10 16:56:08 -0700

    zutaboro

--- EPI/4.1_computing_the_party_of_a_word.py
+++ EPI/4.1_computing_the_party_of_a_word.py
@@ -6,6 +6,8 @@
 # computes the parity of a single 64-bit word.
 # How would you compute the parity of a very large number of 64-bit words?
 
+
+
 # * parity == (量・質・価値など)同等であること，等価
 # odd # of 1s -> parity - 1
 # even # of 1s -> parity - 0 
@@ -14,6 +16,8 @@
 # "10001000" -> 0
 # then
 # "1011","1011","1011" ... ?
+
+# my solution
 def single_word(s):
     counter = 0
     for c in s:
@@ -40,3 +44,29 @@ def many_words(arr):
     else:
         return 1
 print(many_words(["1011","1011","1011"]))
+
+# book solution
+def parity(x):
+    result=0
+    while x:
+        result ^= x&1
+        x>>=1
+    return result
+print("book answer: ", parity(1011))
+
+def parity2(x):
+    result=0
+    while x:
+        result ^= 1
+        x&=x-1 #drops the lowest set bit of x
+    return result
+print("book answer2: ", parity2(1011))
+
+# def parity3(x):
+#     mask_size = 16
+#     bit_mask = 0xFFFF
+#     return (PRECOMPUTED_PARITY[x>>(3*mask_size)]^
+#             PRECOMPUTED_PARITY[(x>>(2*mask_size))&bit_mask]^
+#             PRECOMPUTED_PARITY[(x>>mask_size)
+#                               & bit_mask]^PRECOMPUTED_PARITY[x&bit_mask])
+# print(parity3(1011))
