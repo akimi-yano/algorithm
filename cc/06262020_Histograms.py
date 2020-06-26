8f4451178ed8f947a26986546883b0c169ff41f9
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-06-26 16:58:28 -0700

    HISTOGRAMS - Optimized

--- cc/06262020_Histograms.py
+++ cc/06262020_Histograms.py
@@ -11,54 +11,54 @@
 # (upper-lower)/num_bins
 # (100-20)/8 = 10 in each
 
-class Histogram:
-    def __init__(self,num_bins,lower,upper):
-        self.bins = [0]*num_bins
-        num_each = (upper-lower)//num_bins
-        self.limits = []
-        for i in range(lower+num_each,upper+1,num_each):
-            self.limits.append(i)
-        print("Histgram initialized: ", self.bins)
-        print("Limits initialized: ", self.limits)
+# class Histogram:
+#     def __init__(self,num_bins,lower,upper):
+#         self.bins = [0]*num_bins
+#         num_each = (upper-lower)//num_bins
+#         self.limits = []
+#         for i in range(lower+num_each,upper+1,num_each):
+#             self.limits.append(i)
+#         print("Histgram initialized: ", self.bins)
+#         print("Limits initialized: ", self.limits)
         
-    def count(self,val):
-        def search_category(val):
-            low = 0
-            high = len(self.limits)-1
-            while low<=high:
-                mid = (low+high)//2
-                if self.limits[mid]==val:
-                    return mid
-                elif self.limits[mid]>val:
-                    high=mid-1
-                else:
-                    low=mid+1
-            return low
-        index=search_category(val)
-        self.bins[index]+=1
-    def print_hist(self):
-        print("Histgram updated: ", self.bins)
-        # print("Limits used: ",self.limits)
-my_hist = Histogram(8,20,100)
-new_data = [36,25,38,46,55,68,72,55,36,38,67,45,22,48,91,46,52,61,58,55]
-for data in new_data:
-    my_hist.count(data)
-my_hist.print_hist()
+#     def count(self,val):
+#         def search_category(val):
+#             low = 0
+#             high = len(self.limits)-1
+#             while low<=high:
+#                 mid = (low+high)//2
+#                 if self.limits[mid]==val:
+#                     return mid
+#                 elif self.limits[mid]>val:
+#                     high=mid-1
+#                 else:
+#                     low=mid+1
+#             return low
+#         index=search_category(val)
+#         self.bins[index]+=1
+#     def print_hist(self):
+#         print("Histgram updated: ", self.bins)
+#         # print("Limits used: ",self.limits)
+# my_hist = Histogram(8,20,100)
+# new_data = [36,25,38,46,55,68,72,55,36,38,67,45,22,48,91,46,52,61,58,55]
+# for data in new_data:
+#     my_hist.count(data)
+# my_hist.print_hist()
 
 
-test_data = [30,40,50,60,70,80,90,100]
-def test_search_category(val):
-    low = 0
-    high = len(test_data)-1
-    while low<=high:
-        mid = (low+high)//2
-        if test_data[mid]==val:
-            return mid
-        elif test_data[mid]>val:
-            high=mid-1
-        else:
-            low=mid+1
-    return low
+# test_data = [30,40,50,60,70,80,90,100]
+# def test_search_category(val):
+#     low = 0
+#     high = len(test_data)-1
+#     while low<=high:
+#         mid = (low+high)//2
+#         if test_data[mid]==val:
+#             return mid
+#         elif test_data[mid]>val:
+#             high=mid-1
+#         else:
+#             low=mid+1
+#     return low
 # print(test_search_category(45))
 # print(test_search_category(20))
 # print(test_search_category(30))
@@ -70,3 +70,25 @@ def test_search_category(val):
 # print(test_search_category(46))
 # print(test_search_category(38))
 # print(test_search_category(87))
+
+
+
+# OPTIMIZATION!!! if you know the lower bound and num each, can find the index at O(1)
+# cannot put 100 upper bound is exclusive 
+class Histogram:
+    def __init__(self,num_bins,lower,upper):
+        self.bins = [0]*num_bins
+        self.num_each = (upper-lower)//num_bins
+        self.lower = lower
+            
+    def count(self,val):
+        index = (val - self.lower)//self.num_each 
+        self.bins[index]+=1
+    def print_hist(self):
+        print("Histgram Updated: ", self.bins)
+
+my_hist = Histogram(8,20,100)
+new_data = [36,25,38,46,55,68,72,55,36,38,67,45,22,48,91,46,52,61,58,55]
+for data in new_data:
+    my_hist.count(data)
+my_hist.print_hist()
