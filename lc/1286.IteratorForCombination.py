501ea2f07c26384e80d73c467462aeff598469b7
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-08-13 14:43:31 -0700

    daily

--- lc/1286.IteratorForCombination.py
+++ lc/1286.IteratorForCombination.py
@@ -26,3 +26,34 @@
 # It's guaranteed that all calls of the function next are valid.
 
 
+# This solution works and it is intuitive to me - but there might be better ways to solve this 
+
+class CombinationIterator:
+
+    def __init__(self, characters: str, combinationLength: int):
+        # store all the unique options in self.storage   
+        self.storage = [] # ['ab','ac','bc'] # populate the list with combinationLength using recursion
+        self.chars = characters
+        self.comb_len = combinationLength
+        self.cur = 0
+        self.helper('', 0, self.comb_len, self.storage,0)
+        
+    def helper(self, s, start, target, res, count):
+        if count == target:
+            res.append(s)
+            return 
+        for i in range(start, len(self.chars)):
+            self.helper(s + self.chars[i], i+1, target, res, count+1)
+        
+    def next(self) -> str:
+        self.cur += 1
+        return self.storage[self.cur - 1]
+
+    def hasNext(self) -> bool:
+        return self.cur < len(self.storage) 
+
+
+# Your CombinationIterator object will be instantiated and called as such:
+# obj = CombinationIterator(characters, combinationLength)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
