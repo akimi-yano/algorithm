827db3f06747777f88d95e5849bc294564da5019
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-08-26 17:33:11 -0700

    two pointer approach

--- lc/11.ContainerWithMostWater.py
+++ lc/11.ContainerWithMostWater.py
@@ -40,3 +40,10 @@ class Solution:
             else:
                 res, R = max(res, height[R] * w), R - 1
         return res
+    
+# Explanation:
+# The widest container (using first and last line) is a good candidate, because of its width. 
+# Its water level is the height of the smaller one of first and last line.
+# All other containers are less wide and thus would need a higher water level in order to hold more water.
+# The smaller one of first and last line doesn't support a higher water level and can thus be safely removed 
+# from further consideration.
