c6615028b013d04342ae52b0650018cad7223b59
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-09-16 20:19:09 -0700

    xor

--- lc/421.MaximumXOROfTwoNumbers.py
+++ lc/421.MaximumXOROfTwoNumbers.py
@@ -37,6 +37,7 @@
 #                 best = max(best, nums[i] ^ nums[j])
 #         return best
 
+
 # THIS APPROACH WORKS:
 # trie (binary tree has zero and one children)
 
@@ -123,3 +124,59 @@ class Solution:
                     break
         
         return ans
+
+
+# THIS SOLUTION WORKS AND INTUITIVE TRIES WITH 0s and 1s
+# I know there are more efficient soltions but happy that this works !
+
+class Node:
+    def __init__(self):
+        self.left = None # 0
+        self.right = None # 1
+
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        root = Node()
+        
+        # make trie
+        for num in nums:
+            cur = root
+            for bit in range(30, -1, -1):
+                # if the bit of the num is 1
+                if num & (1 << bit):
+                    if not cur.right:
+                        cur.right = Node()
+                    cur = cur.right 
+                    
+                # if the bit of the num is 0
+                else:
+                    if not cur.left:
+                        cur.left = Node()
+                    cur = cur.left
+        
+        # traverse trie 
+        # if the bit of the num is 1 -> can traverse to 0 (left)
+        # if the bit of the num is 0 -> can traverse to 1 (right)
+        best = 0
+        for num in nums:
+            cur = root
+            xor = 0
+            for bit in range(30, -1, -1):
+                # if the bit of the num is 1
+                if num & (1 << bit):
+                    if cur.left:
+                        xor = (xor << 1) | 1
+                        cur = cur.left
+                    else:
+                        xor = (xor << 1)
+                        cur = cur.right
+                # if the bit of the num is 0
+                else:
+                    if cur.right:
+                        xor = (xor << 1) | 1
+                        cur = cur.right
+                    else:
+                        xor = (xor << 1)
+                        cur = cur.left
+            best = max(best, xor)
+        return best
