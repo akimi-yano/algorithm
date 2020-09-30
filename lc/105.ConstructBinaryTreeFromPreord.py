4272b5d818fd85612a0fbec300ab9bcf3945c14f
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-09-30 11:57:20 -0700

    construct bt

--- lc/105.ConstructBinaryTreeFromPreord.py
+++ lc/105.ConstructBinaryTreeFromPreord.py
@@ -44,3 +44,46 @@
 #             return new_node
 #         return helper(0)
         
+        
+        
+        
+# This solution works !!
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        self.idx_dict = {num: i for i, num in enumerate(inorder)}
+        self.pre_cur_idx = 0
+        
+        def helper(in_start_idx, in_end_idx):
+            if in_end_idx < in_start_idx:
+                return None
+
+            pre_cur_val = preorder[self.pre_cur_idx]
+            self.pre_cur_idx += 1
+            node = TreeNode(pre_cur_val)
+            in_loc = self.idx_dict[pre_cur_val]
+            node.left = helper(in_start_idx, in_loc - 1)
+            node.right = helper(in_loc + 1, in_end_idx)
+            return node
+        
+        return helper(0, len(inorder) - 1)
+    
+'''
+inorder: left root right
+preorder: root left right
+
+we keep moving the index for pre order and for inorder, we find the index of the value in preorder arr at inorder arr and
+its left is the left subtree and its right is the right subtree  
+
+base case:             
+
+if in_end_idx < in_start_idx:
+                return None
+'''
