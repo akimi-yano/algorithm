a4763cce0643088841611f732fe1ce3387275ff8
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-11-07 16:06:58 -0800

    practice seg tree

--- libraries/TreeMaster.py
+++ libraries/TreeMaster.py
@@ -3,10 +3,10 @@ Ki Master Youseikouza
 
 A: 3-1 Union Find - basic x
 B: 3-2 Union Find - optimization x
-C: 4-1 Segment Tree
-D: 4-2 Segment Tree
-E: 4-3 Segment Tree
-F: 4-4 Segment Tree
+C: 4-1 Segment Tree - concept x
+D: 4-2 Segment Tree - one point update range output x
+E: 4-3 Segment Tree - range update one point output x
+F: 4-4 Segment Tree - recursive approach x
 G: 7-1 Splay Tree
 H: 7-2 Splay Tree
 I: 7-3 Splay Tree
@@ -99,17 +99,17 @@ union_find(5,edges)
 
 # D: 4-2 Segment Tree
 '''
-adding the value at the index and return the sum
+adding the value at the index and return the sum of the range
 '''
 
 class SEG_TREE:
     def __init__(self, nums):
-        self.SEG_LEN = 1  << 2
+        self.SEG_LEN = 1  << 18
         self.seg = [0 for _ in range(self.SEG_LEN * 2)]
 
         for i, value in enumerate(nums):
             self.add(i, value)
-            print(self.seg, i, value)
+            # print(self.seg, i, value)
             
     # one point 
     def add(self, idx, val):
@@ -157,5 +157,111 @@ print(seg_tree.sum(0, 2))
 
 # E: 4-3 Segment Tree
 
+'''
+adding the value to all the values within the range and return the value of a point
+'''
+
+class SEG_TREE2:
+    def __init__(self, nums):
+        self.SEG_LEN = 1  << 18
+        self.seg = [0 for _ in range(self.SEG_LEN * 2)]
+
+        for i, value in enumerate(nums):
+            self.add(i, i, value)
+            # print(self.seg, i, value)
+            
+    # one point 
+    def get(self, idx):
+        idx += self.SEG_LEN
+        sum = 0
+        sum += self.seg[idx] 
+        while True:
+            idx //= 2
+            if idx == 0: 
+                break
+            sum += self.seg[idx] 
+        return sum 
+            
+    # range 
+    def add(self, l, r, val):
+        r += 1
+        l += self.SEG_LEN
+        r += self.SEG_LEN
+        ans = 0
+        while l < r:
+            if l %  2  == 1:
+                self.seg[l] += val
+                l+=1
+            l//=2
+            if r %  2  == 1:
+                self.seg[r-1] += val 
+                r-=1
+            r//=2
+        
+seg_tree2 = SEG_TREE2([1, 3, 5])
+# Given nums = [1, 3, 5]
+
+print(seg_tree2.get(0))
+print(seg_tree2.get(1))
+print(seg_tree2.get(2))
+
+seg_tree2.add(0,2,-5)
+
+print(seg_tree2.get(0))
+print(seg_tree2.get(1))
+print(seg_tree2.get(2))
+
+
 
 # F: 4-4 Segment Tree
+'''
+recursive approach
+set val (one point) and get min (range)
+'''
+
+class SEG_TREE3:
+    def __init__(self, nums):
+        self.SEG_LEN = 1  << 3
+        self.seg = [0 for _ in range(self.SEG_LEN * 2)]
+
+        for i, value in enumerate(nums):
+            print(i, value)
+            self.set_val(i, value)
+            # print(self.seg, i, value)
+            
+    # one point 
+    def set_val(self, pos, val):
+        pos += 1 << 2
+        self.seg[pos] = val
+        while True:
+            pos//=2
+            if pos < 1:
+                break
+            self.seg[pos] = min(self.seg[pos*2], self.seg[pos*2+1])
+            
+    # range 
+    def get_min(self, ql, qr, sl = 0 , sr = 1 << 2, pos =  1):
+        # chittomo kabutte nai 
+        if  qr <= sl or sr <= ql:
+            return float('inf')
+        # kannzennni tsutsumareteru 
+        if ql <= sl and sr <= qr:
+            return self.seg[pos]
+        sm = (sl + sr) //2 
+        lmin = self.get_min(ql, qr, sl, sm, pos * 2)
+        rmin = self.get_min(ql, qr, sm, sr, pos * 2 + 1)
+        return min(lmin, rmin)
+
+
+seg_tree3 = SEG_TREE3([1, 3, 5])
+# Given nums = [1, 3, 5]
+print(seg_tree3.seg)
+print(seg_tree3.get_min(0,2+1))
+print(seg_tree3.set_val(1,-1))
+print(seg_tree3.seg)
+print(seg_tree3.get_min(0,2+1))
+print(seg_tree3.set_val(2,-2))
+print(seg_tree3.seg)
+print(seg_tree3.get_min(0,2+1))
+print(seg_tree3.get_min(1,1+1))
+
