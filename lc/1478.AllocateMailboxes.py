4b4387d04707ef00262fc7ca4095be2afcb70507
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-11-09 21:39:13 -0800

    optimization

--- lc/1478.AllocateMailboxes.py
+++ lc/1478.AllocateMailboxes.py
@@ -94,3 +94,45 @@ class Solution:
         ans = sum(abs(median-self.houses[i]) for i in range(start, end+1))
         self.memo2[key] = ans
         return ans
+    
+
+# This solution works ! - optimization 
+'''
+middle index is just (start+end)//2 
+'''
+
+class Solution:
+    def minDistance(self, houses: List[int], k: int) -> int:
+        self.memo = {}
+        self.memo2 = {}
+        self.N = len(houses)
+        self.houses = houses
+        self.houses.sort()
+
+        return self.helper(0, k)
+    
+    def helper(self, i, remaining):
+        key = (i, remaining)
+        if key in self.memo:
+            return self.memo[key]
+
+        ans = float('inf')
+        if i >= self.N:
+            ans = 0
+        elif remaining < 1:
+            pass
+        else:
+            for j in range(i, self.N-remaining+1):
+                ans = min(ans, self.helper(j+1, remaining-1) + self.distance(i, j))
+
+        self.memo[key] = ans
+        return ans
+    
+    def distance(self, start, end):
+        key = (start, end)
+        if key in self.memo2:
+            return self.memo2[key]
+        middle = self.houses[(start+end)//2]
+        ans = sum(abs(middle-self.houses[i]) for i in range(start, end+1))
+        self.memo2[key] = ans
+        return ans
