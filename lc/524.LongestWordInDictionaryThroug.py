45f84526916286c6a5008851f026c9de787c8545
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-02-22 14:46:40 -0800

    dictionary

---
+++ lc/524.LongestWordInDictionaryThroug.py
@@ -0,0 +1,52 @@
+# 524. Longest Word in Dictionary through Deleting
+# Medium
+
+# 867
+
+# 282
+
+# Add to List
+
+# Share
+# Given a string and a string dictionary, find the longest string in the dictionary that can be formed by deleting some characters of the given string. If there are more than one possible results, return the longest word with the smallest lexicographical order. If there is no possible result, return the empty string.
+
+# Example 1:
+# Input:
+# s = "abpcplea", d = ["ale","apple","monkey","plea"]
+
+# Output: 
+# "apple"
+# Example 2:
+# Input:
+# s = "abpcplea", d = ["a","b","c"]
+
+# Output: 
+# "a"
+# Note:
+# All the strings in the input will only contain lower-case letters.
+# The size of the dictionary won't exceed 1,000.
+# The length of all the strings in the input won't exceed 1,000.
+
+# This solution works:
+
+class Solution:
+    def findLongestWord(self, s: str, d: List[str]) -> str:
+        longest = 0
+        ans = ""
+        for word in d:
+            idx1 = idx2 = 0
+            while idx1 < len(word) and idx2 < len(s):
+                if word[idx1] == s[idx2]:
+                    idx1 +=1
+                    idx2 +=1
+                else:
+                    idx2+=1
+            if idx1 >= len(word):
+                if longest < len(word):
+                    longest = len(word)
+                    ans = word
+                elif longest == len(word):
+                    longest = len(word)
+                    if not ans or ans > word:
+                        ans = word
+        return ans
