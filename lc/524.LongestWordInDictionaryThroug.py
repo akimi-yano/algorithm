517788eb388da93428a2599c412c04466321b7b0
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-02-22 20:56:48 -0800

    optimization

--- lc/524.LongestWordInDictionaryThroug.py
+++ lc/524.LongestWordInDictionaryThroug.py
@@ -27,6 +27,7 @@
 # The size of the dictionary won't exceed 1,000.
 # The length of all the strings in the input won't exceed 1,000.
 
+
 # This solution works:
 
 class Solution:
@@ -34,6 +35,36 @@ class Solution:
         longest = 0
         ans = ""
         for word in d:
+            idx1 = idx2 = 0
+            while idx1 < len(word) and idx2 < len(s):
+                if word[idx1] == s[idx2]:
+                    idx1 +=1
+                    idx2 +=1
+                else:
+                    idx2+=1
+            if idx1 >= len(word):
+                if longest < len(word):
+                    longest = len(word)
+                    ans = word
+                elif longest == len(word):
+                    longest = len(word)
+                    if not ans or ans > word:
+                        ans = word
+        return ans
+    
+    
+# This solution works - optimization - no need to check if best length is larger than the current one or 
+# best length == current length but the answer word is smaller than the current word:
+
+class Solution:
+    def findLongestWord(self, s: str, d: List[str]) -> str:
+        longest = 0
+        ans = ""
+        for word in d:
+            if len(word) < longest:
+                continue
+            elif len(word) == longest and ans < word:
+                continue
             idx1 = idx2 = 0
             while idx1 < len(word) and idx2 < len(s):
                 if word[idx1] == s[idx2]:
