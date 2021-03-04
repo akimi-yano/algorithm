2a4a3c9ffff3aa4c70d21aa6f79f6efc22006610
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-03-03 22:39:55 -0800

    roman

--- lc/12.IntegerToRoman.py
+++ lc/12.IntegerToRoman.py
@@ -95,4 +95,15 @@ class Solution:
             num //= 10
         ans.reverse()
         return "".join(ans)
-        
+
+# This solution works:
+
+class Solution(object):
+    def intToRoman(self, num):
+        dict = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+        nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        result = ""
+        for letter, n in zip(dict, nums):
+            result += letter * int(num / n)
+            num %= n
+        return result
