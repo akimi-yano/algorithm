e4a9d807704091876683900097d334e3b86c51af
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-04-16 23:54:12 -0700

    optimization

--- lc/1209.RemoveAllAdjacentDuplicatesii.py
+++ lc/1209.RemoveAllAdjacentDuplicatesii.py
@@ -60,3 +60,19 @@
 #         return "".join(stack)
                     
 
+# This solution works:
+
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = [[None, 1]]
+        for c in s:
+            if stack[-1][0] == c:
+                stack[-1][1] += 1
+            else:
+                stack.append([c, 1])
+            stack[-1][1] =  stack[-1][1] % k
+            if stack[-1][1] == 0:
+                stack.pop()
+        
+        return "".join([char * freq for char, freq in stack[1:]])
+                    
