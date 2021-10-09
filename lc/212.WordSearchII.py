576004b66bec0ab02358ee088b9b37da3a4cc7ea
Akimi Yano <akimi.mimi.yano@gmail.com>
2021-10-08 22:21:16 -0700

    trie implementation

---
+++ lc/212.WordSearchII.py
@@ -0,0 +1,94 @@
+#  212. Word Search II
+# Hard
+
+# 4594
+
+# 158
+
+# Add to List
+
+# Share
+# Given an m x n board of characters and a list of strings words, return all words on the board.
+
+# Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+
+ 
+
+# Example 1:
+
+
+# Input: board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], words = ["oath","pea","eat","rain"]
+# Output: ["eat","oath"]
+# Example 2:
+
+
+# Input: board = [["a","b"],["c","d"]], words = ["abcb"]
+# Output: []
+ 
+
+# Constraints:
+
+# m == board.length
+# n == board[i].length
+# 1 <= m, n <= 12
+# board[i][j] is a lowercase English letter.
+# 1 <= words.length <= 3 * 104
+# 1 <= words[i].length <= 10
+# words[i] consists of lowercase English letters.
+# All the strings of words are unique.
+
+
+# This solution works - Trie:
+
+
+class TrieNode():
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.isWord = False
+    
+class Trie():
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for w in word:
+            node = node.children[w]
+        node.isWord = True
+    
+    def search(self, word):
+        node = self.root
+        for w in word:
+            node = node.children.get(w)
+            if not node:
+                return False
+        return node.isWord
+    
+class Solution(object):
+    def findWords(self, board, words):
+        res = []
+        trie = Trie()
+        node = trie.root
+        for w in words:
+            trie.insert(w)
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                self.dfs(board, node, i, j, "", res)
+        return res
+    
+    def dfs(self, board, node, i, j, path, res):
+        if node.isWord:
+            res.append(path)
+            node.isWord = False
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+            return 
+        tmp = board[i][j]
+        node = node.children.get(tmp)
+        if not node:
+            return 
+        board[i][j] = "#"
+        self.dfs(board, node, i+1, j, path+tmp, res)
+        self.dfs(board, node, i-1, j, path+tmp, res)
+        self.dfs(board, node, i, j-1, path+tmp, res)
+        self.dfs(board, node, i, j+1, path+tmp, res)
+        board[i][j] = tmp
