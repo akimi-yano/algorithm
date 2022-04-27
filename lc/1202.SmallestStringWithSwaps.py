44fb778fb9d232479ded072160399cf4ccb7ba01
Akimi Yano <akimi.mimi.yano@gmail.com>
2022-04-26 23:15:27 -1000

    swaps

---
+++ lc/1202.SmallestStringWithSwaps.py
@@ -0,0 +1,86 @@
+# 1202. Smallest String With Swaps
+# Medium
+
+# 2029
+
+# 61
+
+# Add to List
+
+# Share
+# You are given a string s, and an array of pairs of indices in the string pairs where pairs[i] = [a, b] indicates 2 indices(0-indexed) of the string.
+
+# You can swap the characters at any pair of indices in the given pairs any number of times.
+
+# Return the lexicographically smallest string that s can be changed to after using the swaps.
+
+ 
+
+# Example 1:
+
+# Input: s = "dcab", pairs = [[0,3],[1,2]]
+# Output: "bacd"
+# Explaination: 
+# Swap s[0] and s[3], s = "bcad"
+# Swap s[1] and s[2], s = "bacd"
+# Example 2:
+
+# Input: s = "dcab", pairs = [[0,3],[1,2],[0,2]]
+# Output: "abcd"
+# Explaination: 
+# Swap s[0] and s[3], s = "bcad"
+# Swap s[0] and s[2], s = "acbd"
+# Swap s[1] and s[2], s = "abcd"
+# Example 3:
+
+# Input: s = "cba", pairs = [[0,1],[1,2]]
+# Output: "abc"
+# Explaination: 
+# Swap s[0] and s[1], s = "bca"
+# Swap s[1] and s[2], s = "bac"
+# Swap s[0] and s[1], s = "abc"
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 10^5
+# 0 <= pairs.length <= 10^5
+# 0 <= pairs[i][0], pairs[i][1] < s.length
+# s only contains lower case English letters.
+
+
+# This solution works:
+
+
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        class UF:
+            def __init__(self, n): self.p = list(range(n))
+            def union(self, x, y): self.p[self.find(x)] = self.find(y)
+            def find(self, x):
+                if x != self.p[x]: self.p[x] = self.find(self.p[x])
+                return self.p[x]
+        uf, res, m = UF(len(s)), [], defaultdict(list)
+        for x,y in pairs: 
+            uf.union(x,y)
+        for i in range(len(s)): 
+            m[uf.find(i)].append(s[i])
+        for comp_id in m.keys(): 
+            m[comp_id].sort(reverse=True)
+        for i in range(len(s)): 
+            res.append(m[uf.find(i)].pop())
+        return ''.join(res)
+        '''
+        The core of the idea is that if (0, 1) is an exchange pair and (0, 2) is an exchange pair, then any 2 in (0, 1, 2) can be exchanged.
+
+This implies, we can build connected components where each component is a list of indices that can be exchanged with any of them. In Union find terms, we simply iterate through each pair, and do a union on the indices in the pair.
+At the end of the union of all the pairs, we have built connected component of indices that can be exchanged with each other.
+
+Then we build a sorted list of characters for every connected component.
+
+The final step is, we iterate through all the indices, and for each index we locate its component id and find the sorted list correspondng to that component and grab the next lowest character from that list.
+
+This way for every index, we find the lowest possible character that can be exchanged and fitted there.
+
+
+        '''
