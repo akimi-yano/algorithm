5676c0f3068405efc319f6be518127cdfea69b4f
Akimi Yano <akimi.mimi.yano@gmail.com>
2022-05-09 21:59:53 -0700

    comb sum

---
+++ lc/review_216.CombinationSumIII.py
@@ -0,0 +1,70 @@
+# 216. Combination Sum III
+# Medium
+
+# 2967
+
+# 76
+
+# Add to List
+
+# Share
+# Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+
+# Only numbers 1 through 9 are used.
+# Each number is used at most once.
+# Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
+
+ 
+
+# Example 1:
+
+# Input: k = 3, n = 7
+# Output: [[1,2,4]]
+# Explanation:
+# 1 + 2 + 4 = 7
+# There are no other valid combinations.
+# Example 2:
+
+# Input: k = 3, n = 9
+# Output: [[1,2,6],[1,3,5],[2,3,4]]
+# Explanation:
+# 1 + 2 + 6 = 9
+# 1 + 3 + 5 = 9
+# 2 + 3 + 4 = 9
+# There are no other valid combinations.
+# Example 3:
+
+# Input: k = 4, n = 1
+# Output: []
+# Explanation: There are no valid combinations.
+# Using 4 different numbers in the range [1,9], the smallest sum we can get is 1+2+3+4 = 10 and since 10 > 1, there are no valid combination.
+ 
+
+# Constraints:
+
+# 2 <= k <= 9
+# 1 <= n <= 60
+
+
+# This solution works:
+
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        def helper(arr, num):
+            nonlocal ans
+            if sum(arr) == n and len(arr) == k:
+                ans.append(arr)
+                return
+            if num > 9:
+                return
+            if sum(arr) > n:
+                return 
+            if len(arr) > k:
+                return
+
+            helper(arr+[num], num+1)
+            helper(arr, num+1)
+        ans = []    
+        helper([], 1)
+        return ans
