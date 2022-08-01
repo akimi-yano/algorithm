b8655f4ecdcf5bdf582024cb2d789d48829b7a39
Akimi Yano <akimi.mimi.yano@gmail.com>
2022-07-31 21:20:55 -0700

    unique paths

---
+++ lc/review_62.UniquePaths.py
@@ -0,0 +1,52 @@
+# 62. Unique Paths
+# Medium
+
+# 10242
+
+# 319
+
+# Add to List
+
+# Share
+# There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+# Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+# The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+ 
+
+# Example 1:
+
+
+# Input: m = 3, n = 7
+# Output: 28
+# Example 2:
+
+# Input: m = 3, n = 2
+# Output: 3
+# Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+# 1. Right -> Down -> Down
+# 2. Down -> Down -> Right
+# 3. Down -> Right -> Down
+ 
+
+# Constraints:
+
+# 1 <= m, n <= 100
+
+
+# This solution works:
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        @lru_cache(None)
+        def helper(row, col):
+            nonlocal m, n
+            if not(0<=row<m) or not(0<=col<n):
+                return 0
+            if row == m-1 and col == n-1:
+                return 1
+            return helper(row+1, col) + helper(row, col+1)
+        return helper(0, 0)
