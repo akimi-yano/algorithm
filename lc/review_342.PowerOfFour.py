49088a46fdbc7ea2494dc12baae2dde3c2743190
Akimi Yano <akimi.mimi.yano@gmail.com>
2022-08-21 19:23:58 -0700

    daily

---
+++ lc/review_342.PowerOfFour.py
@@ -0,0 +1,47 @@
+# 342. Power of Four
+# Easy
+
+# 1768
+
+# 295
+
+# Add to List
+
+# Share
+# Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+# An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+ 
+
+# Example 1:
+
+# Input: n = 16
+# Output: true
+# Example 2:
+
+# Input: n = 5
+# Output: false
+# Example 3:
+
+# Input: n = 1
+# Output: true
+ 
+
+# Constraints:
+
+# -231 <= n <= 231 - 1
+
+
+# This solution works:
+
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n < 1:
+            return False
+        if n == 1:
+            return True
+        if n % 4:
+            return False
+        return self.isPowerOfFour(n // 4)
