e1b2df31b05e6dea9cb81e53f38cdd0f0c49a6b4
Akimi Yano <akimi.mimi.yano@gmail.com>
2022-08-27 00:00:50 -0700

    ans

--- lc/review_363.MaxSumOfRectangleNoLargerTh.py
+++ lc/review_363.MaxSumOfRectangleNoLargerTh.py
@@ -36,3 +36,27 @@
  
 
 # Follow up: What if the number of rows is much larger than the number of columns?
+
+
+# This solution works:
+
+
+class Solution:
+    def maxSumSubmatrix(self, M, k):
+        
+        def countRangeSum(nums, U):
+            SList, ans = [0], -float("inf")
+            for s in accumulate(nums):
+                idx = bisect_left(SList, s - U) 
+                if idx < len(SList): ans = max(ans, s - SList[idx])        
+                bisect.insort(SList, s)
+            return ans
+        
+        m, n, ans = len(M), len(M[0]), -float("inf")
+        for i, j in product(range(1, m), range(n)):
+            M[i][j] += M[i-1][j]
+        M = [[0]*n] + M
+        for r1, r2 in combinations(range(m + 1), 2):
+            row = [j - i for i, j in zip(M[r1], M[r2])]
+            ans = max(ans, countRangeSum(row, k))
+        return ans
