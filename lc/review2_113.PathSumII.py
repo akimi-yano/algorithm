e15f2990dc662546911e5f0ec733c8e453713ab6
Akimi Yano <akimi.mimi.yano@gmail.com>
2022-09-23 22:39:15 -0700

    path sum

---
+++ lc/review2_113.PathSumII.py
@@ -0,0 +1,69 @@
+# 113. Path Sum II
+# Medium
+
+# 5707
+
+# 122
+
+# Add to List
+
+# Share
+# Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+# A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+ 
+
+# Example 1:
+
+
+# Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+# Output: [[5,4,11,2],[5,8,4,5]]
+# Explanation: There are two paths whose sum equals targetSum:
+# 5 + 4 + 11 + 2 = 22
+# 5 + 8 + 4 + 5 = 22
+# Example 2:
+
+
+# Input: root = [1,2,3], targetSum = 5
+# Output: []
+# Example 3:
+
+# Input: root = [1,2], targetSum = 0
+# Output: []
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 5000].
+# -1000 <= Node.val <= 1000
+# -1000 <= targetSum <= 1000
+
+
+# This solution works:
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        def helper(cur, paths, remaining):
+            nonlocal ans
+            if not cur.left and not cur.right:
+                if remaining-cur.val == 0:
+                    ans.append(paths+[cur.val])
+                return
+            if cur.left:
+                helper(cur.left, paths+[cur.val], remaining-cur.val)
+            if cur.right:
+                helper(cur.right, paths+[cur.val], remaining-cur.val)
+        
+        ans = []
+        if not root:
+            return ans
+        helper(root, [], targetSum)
+        return ans
