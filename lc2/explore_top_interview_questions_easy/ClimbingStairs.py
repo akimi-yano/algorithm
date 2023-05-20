def8e38ca68c73651552b3763e5eb465d707689d
Akimi Yano <akimi.mimi.yano@gmail.com>
2023-05-19 23:21:36 -0700

    review

---
+++ lc2/explore_top_interview_questions_easy/ClimbingStairs.py
@@ -0,0 +1,61 @@
+'''
+70. Climbing Stairs
+Easy
+18.1K
+569
+Companies
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+'''
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        '''
+        either climb 1 or 2 step to get to n
+        3
+        
+        helper(2) + helper(1)
+        helper(2-1=1) + helper(2-2=0) + helper(1)
+        
+        2 
+        helper(2)
+        
+        '''
+        
+        def helper(n):
+            if n in memo:
+                return memo[n]
+            if n == 1:
+                return 1
+            elif n == 2:
+                return 2
+            total = helper(n-1) + helper(n-2)
+            memo[n] = total 
+            return memo[n]
+        memo = {}
+        return helper(n)
---
+++ lc2/explore_top_interview_questions_easy/TwoSum.py
@@ -0,0 +1,79 @@
+'''
+1. Two Sum
+Easy
+46.4K
+1.5K
+Companies
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        [2,7,11,15] 9
+        memo {
+        2, [0]
+        7, [1]
+        11, [2]
+        15, [3]
+        }
+        
+        
+        [3,2,4], target = 6
+        memo {
+        3, [0]
+        2, [1]
+        4, [2]
+        }
+        
+        nums = [3,3], target = 6
+        memo {
+        3, [0, 1]
+        }
+        '''
+        memo = {}
+        for i, num in enumerate(nums):
+            if num not in memo:
+                memo[num] = []
+            memo[num].append(i) 
+        
+        for i, num in enumerate(nums):
+            look = target-num
+            if num == look:
+                if len(memo[look]) >= 2:
+                    return memo[look]
+            else:
+                if look in memo:
+                    return [i, memo[look][0]]
+        
