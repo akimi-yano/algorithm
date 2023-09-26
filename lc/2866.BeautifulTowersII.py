9a0c58343beac516648ca1822b8a385b12f5422b
Akimi Yano <akimi.mimi.yano@gmail.com>
2023-09-26 10:09:51 -0700

    coding this again

--- lc/2866.BeautifulTowersII.py
+++ lc/2866.BeautifulTowersII.py
@@ -85,3 +85,60 @@ This tower is composed of sections, each section has height maxHeights[i] and wi
 
 In particular, we destroy any section that is higher than maxHeights[i] by popping from the stack, then we construct a new section with width. total keeps track of the total area of the tower.
 '''
+
+
+class Solution:
+    def maximumSumOfHeights(self, maxHeights):
+        '''
+        [5,3,4,1,1]
+         0 1 2 3 4
+
+         if 2 is the peak:
+             itsLeftIfiIsPeak : [3 3 4 <- strictly increasing
+             itsReftIfiIsPeak : 4 1 1] <- strictly decreasing (can reverse)
+            3+3+4+1+1 (+4-4 as we added the peak twice) 
+        '''
+        N = len(maxHeights)
+
+        # LEFT SIDE:
+        itsLeftAndSelfIfiIsPeak = [] # save the total of left side if the peak is at i :[] -> [5] -> [5, 6] -> [5, 6, 10]
+        monostack = [] # strickly increasing value's i and width :[] -> [0] -> [1] -> [1,2] -> [0,1] -> [0] * width is ommited here in this comment
+        leftTotalIfiIsPeak = 0 
+        for i in range(N): # : 0
+            width = 1
+            while monostack and maxHeights[i] < maxHeights[monostack[-1][0]]:
+                # : monostack is empty
+                j, w = monostack.pop()
+                leftTotalIfiIsPeak -= maxHeights[j] * w
+                width += w
+                
+            leftTotalIfiIsPeak += maxHeights[i] * width
+            itsLeftAndSelfIfiIsPeak.append(leftTotalIfiIsPeak)
+            monostack.append([i, width])
+        print(itsLeftAndSelfIfiIsPeak)
+
+        # RIGHT SIDE:
+        itsRightAndSelfIfiIsPeak = [] 
+        monostack = [] # strickly increasing value's i and width
+        rightTotalIfiIsPeak = 0 
+        maxHeights = maxHeights[::-1]
+        for i in range(N): 
+            width = 1
+            while monostack and maxHeights[i] < maxHeights[monostack[-1][0]]:
+                # : monostack is empty
+                j, w = monostack.pop()
+                rightTotalIfiIsPeak -= maxHeights[j] * w
+                width += w
+                
+            rightTotalIfiIsPeak += maxHeights[i] * width
+            itsRightAndSelfIfiIsPeak.append(rightTotalIfiIsPeak)
+            monostack.append([i, width])
+        itsRightAndSelfIfiIsPeak = itsRightAndSelfIfiIsPeak[::-1]
+        
+        # CALCULATE THE TOTAL:
+        maxHeights = maxHeights[::-1]
+        best = 0
+        for k in range(N):
+            total = itsLeftAndSelfIfiIsPeak[k]+itsRightAndSelfIfiIsPeak[k]-maxHeights[k]
+            best = max(best, total)
+        return best
