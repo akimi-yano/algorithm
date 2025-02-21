3e15a528932a8241ae81610165b49ab633a8ea87
Akimi Yano <akimi.mimi.yano@gmail.com>
2025-02-21 12:37:20 -0800

    tree

---
+++ lc_top150_daily_comp_2024/1261.FindElementsinaContaminatedBinaryTree.py
@@ -0,0 +1,140 @@
+'''
+1261. Find Elements in a Contaminated Binary Tree
+Solved
+Medium
+Topics
+Companies
+Hint
+Given a binary tree with the following rules:
+
+root.val == 0
+For any treeNode:
+If treeNode.val has a value x and treeNode.left != null, then treeNode.left.val == 2 * x + 1
+If treeNode.val has a value x and treeNode.right != null, then treeNode.right.val == 2 * x + 2
+Now the binary tree is contaminated, which means all treeNode.val have been changed to -1.
+
+Implement the FindElements class:
+
+FindElements(TreeNode* root) Initializes the object with a contaminated binary tree and recovers it.
+bool find(int target) Returns true if the target value exists in the recovered binary tree.
+ 
+
+Example 1:
+
+
+Input
+["FindElements","find","find"]
+[[[-1,null,-1]],[1],[2]]
+Output
+[null,false,true]
+Explanation
+FindElements findElements = new FindElements([-1,null,-1]); 
+findElements.find(1); // return False 
+findElements.find(2); // return True 
+Example 2:
+
+
+Input
+["FindElements","find","find","find"]
+[[[-1,-1,-1,-1,-1]],[1],[3],[5]]
+Output
+[null,true,true,false]
+Explanation
+FindElements findElements = new FindElements([-1,-1,-1,-1,-1]);
+findElements.find(1); // return True
+findElements.find(3); // return True
+findElements.find(5); // return False
+Example 3:
+
+
+Input
+["FindElements","find","find","find","find"]
+[[[-1,null,-1,-1,null,-1]],[2],[3],[4],[5]]
+Output
+[null,true,false,false,true]
+Explanation
+FindElements findElements = new FindElements([-1,null,-1,-1,null,-1]);
+findElements.find(2); // return True
+findElements.find(3); // return False
+findElements.find(4); // return False
+findElements.find(5); // return True
+ 
+
+Constraints:
+
+TreeNode.val == -1
+The height of the binary tree is less than or equal to 20
+The total number of nodes is between [1, 104]
+Total calls of find() is between [1, 104]
+0 <= target <= 106
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        def update_val(node, val):
+            if not node:
+                return 
+            node.val = val
+            update_val(node.left, 2*val+1)
+            update_val(node.right, 2*val+2)
+        
+        update_val(root, 0)
+        self.root = root
+
+    def find(self, target: int) -> bool:
+        def traverse(node):
+            nonlocal target
+            if not node:
+                return False
+            if node.val == target:
+                return True
+            if node.val > target:
+                return False
+            return traverse(node.left) or traverse(node.right)
+
+        return traverse(self.root)
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
+
+# Optimization: constant time look up
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.seen = set([])
+
+        def update_val(node, val):
+            if not node:
+                return 
+            node.val = val
+            self.seen.add(val)
+            update_val(node.left, 2*val+1)
+            update_val(node.right, 2*val+2)
+        
+        update_val(root, 0)
+
+    def find(self, target: int) -> bool:
+        return target in self.seen
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
+
+
+# __init__() - Time: O(N) Space: O(N)
+# find() - Time: O(1) Space: O(1)
