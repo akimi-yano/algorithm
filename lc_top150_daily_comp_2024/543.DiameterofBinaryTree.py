4a9c7b06104abd3e3db8a2e79fed9ba995de0ccc
Akimi Yano <akimi.mimi.yano@gmail.com>
2025-02-21 16:33:56 -0800

    tree

---
+++ lc_top150_daily_comp_2024/543.DiameterofBinaryTree.py
@@ -0,0 +1,95 @@
+'''
+543. Diameter of Binary Tree
+Solved
+Easy
+Topics
+Companies
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+Example 2:
+
+Input: root = [1,2]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-100 <= Node.val <= 100
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        max_edge_length = 0
+
+        def helper(cur):
+            nonlocal max_edge_length
+            
+            if not cur.left and not cur.right:
+                return 0
+
+            left_edge = 0
+            right_edge = 0
+
+            if cur.left:
+                left_edge = 1 + helper(cur.left)
+            
+            if cur.right:
+                right_edge = 1 + helper(cur.right)
+            
+            max_edge_length = max(max_edge_length, left_edge + right_edge)
+            return max(left_edge, right_edge)
+        
+        helper(root)
+        return max_edge_length
+            
+# Simplified a little:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        max_edge_length = 0
+
+        def helper(cur):
+            nonlocal max_edge_length
+
+            left_edge = 0
+            right_edge = 0
+
+            if cur.left:
+                left_edge = 1 + helper(cur.left)
+            
+            if cur.right:
+                right_edge = 1 + helper(cur.right)
+            
+            max_edge_length = max(max_edge_length, left_edge + right_edge)
+            return max(left_edge, right_edge)
+        
+        helper(root)
+        return max_edge_length
+            
+# Time: O(N) where N is the number of all the nodes
+# Space: O(N) where N is the number of the call stacks which is the same as the number of nodes
