df966dadcd1aa506c57615e9c0705a687d2b2a83
Akimi Yano <akimi.mimi.yano@gmail.com>
2025-03-04 16:13:20 -0800

    power of three

---
+++ lc_top150_daily_comp_2024/1780.CheckifNumberisaSumofPowersofThree.py
@@ -0,0 +1,43 @@
+'''
+1780. Check if Number is a Sum of Powers of Three
+Medium
+Topics
+Companies
+Hint
+Given an integer n, return true if it is possible to represent n as the sum of distinct powers of three. Otherwise, return false.
+
+An integer y is a power of three if there exists an integer x such that y == 3x.
+
+ 
+
+Example 1:
+
+Input: n = 12
+Output: true
+Explanation: 12 = 31 + 32
+Example 2:
+
+Input: n = 91
+Output: true
+Explanation: 91 = 30 + 32 + 34
+Example 3:
+
+Input: n = 21
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 107
+'''
+
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        if n == 1:
+            return True
+        if n % 3 == 2:
+            return False
+        return self.checkPowersOfThree(n//3)
+'''
+The remainders can be 0,1 or 2. So if at all the remainder is 2, we cannot represent n as sum of power 3.
+'''
