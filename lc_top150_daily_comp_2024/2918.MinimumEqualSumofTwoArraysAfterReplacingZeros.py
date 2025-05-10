05144269988a6c1bd8261da7df66273f19e8cc93
Akimi Yano <akimi.mimi.yano@gmail.com>
2025-05-11 07:01:28 +0900

    sma

---
+++ lc_top150_daily_comp_2024/2918.MinimumEqualSumofTwoArraysAfterReplacingZeros.py
@@ -0,0 +1,44 @@
+'''
+2918. Minimum Equal Sum of Two Arrays After Replacing Zeros
+Medium
+Topics
+Companies
+Hint
+You are given two arrays nums1 and nums2 consisting of positive integers.
+
+You have to replace all the 0's in both arrays with strictly positive integers such that the sum of elements of both arrays becomes equal.
+
+Return the minimum equal sum you can obtain, or -1 if it is impossible.
+
+ 
+
+Example 1:
+
+Input: nums1 = [3,2,0,1,0], nums2 = [6,5,0]
+Output: 12
+Explanation: We can replace 0's in the following way:
+- Replace the two 0's in nums1 with the values 2 and 4. The resulting array is nums1 = [3,2,2,1,4].
+- Replace the 0 in nums2 with the value 1. The resulting array is nums2 = [6,5,1].
+Both arrays have an equal sum of 12. It can be shown that it is the minimum sum we can obtain.
+Example 2:
+
+Input: nums1 = [2,0,2,0], nums2 = [1,4]
+Output: -1
+Explanation: It is impossible to make the sum of both arrays equal.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 105
+0 <= nums1[i], nums2[i] <= 106
+'''
+
+class Solution:
+    def minSum(self, nums1: List[int], nums2: List[int]) -> int:
+        total1 = sum(max(num1, 1) for num1 in nums1)
+        total2 = sum(max(num2, 1) for num2 in nums2)
+        if total1 < total2 and nums1.count(0) == 0:
+            return -1
+        if total1 > total2 and nums2.count(0) == 0:
+            return -1
+        return max(total1, total2)
