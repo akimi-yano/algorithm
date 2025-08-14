54743b63ef01c780da042cfcf9595b2374a73636
Akimi Yano <akimi.mimi.yano@gmail.com>
2025-08-15 04:17:35 +0900

    largest 3

---
+++ lc_top150_daily_comp_2024/2264.Largest3-Same-DigitNumberinString.py
@@ -0,0 +1,60 @@
+'''
+2264. Largest 3-Same-Digit Number in String
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+
+It is a substring of num with length 3.
+It consists of only one unique digit.
+Return the maximum good integer as a string or an empty string "" if no such integer exists.
+
+Note:
+
+A substring is a contiguous sequence of characters within a string.
+There may be leading zeroes in num or a good integer.
+ 
+
+Example 1:
+
+Input: num = "6777133339"
+Output: "777"
+Explanation: There are two distinct good integers: "777" and "333".
+"777" is the largest, so we return "777".
+Example 2:
+
+Input: num = "2300019"
+Output: "000"
+Explanation: "000" is the only good integer.
+Example 3:
+
+Input: num = "42352338"
+Output: ""
+Explanation: No substring of length 3 consists of only one unique digit. Therefore, there are no good integers.
+ 
+
+Constraints:
+
+3 <= num.length <= 1000
+num only consists of digits.
+'''
+
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        look = [str(n)+str(n)+str(n) for n in range(9,-1,-1)]
+        for elem in look:
+            if elem in num:
+                return elem
+        return ""
+    
+
+# Another approach:
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        best = ""
+        for i in range(len(num)-2):
+            if num[i] == num[i+1] == num[i+2]:
+                best = max(best, num[i:i+3])
+        return best
