dc06001e1b15924581562eed4e2ed55c2bdcfe33
Akimi Yano <akimi.mimi.yano@gmail.com>
2025-08-19 09:00:52 +0900

    game

---
+++ lc_top150_daily_comp_2024/679.24Game.py
@@ -0,0 +1,56 @@
+'''
+679. 24 Game
+Hard
+Topics
+premium lock icon
+Companies
+You are given an integer array cards of length 4. You have four cards, each containing a number in the range [1, 9]. You should arrange the numbers on these cards in a mathematical expression using the operators ['+', '-', '*', '/'] and the parentheses '(' and ')' to get the value 24.
+
+You are restricted with the following rules:
+
+The division operator '/' represents real division, not integer division.
+For example, 4 / (1 - 2 / 3) = 4 / (1 / 3) = 12.
+Every operation done is between two numbers. In particular, we cannot use '-' as a unary operator.
+For example, if cards = [1, 1, 1, 1], the expression "-1 - 1 - 1 - 1" is not allowed.
+You cannot concatenate numbers together
+For example, if cards = [1, 2, 1, 2], the expression "12 + 12" is not valid.
+Return true if you can get such expression that evaluates to 24, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: cards = [4,1,8,7]
+Output: true
+Explanation: (8-4) * (7-1) = 24
+Example 2:
+
+Input: cards = [1,2,1,2]
+Output: false
+ 
+
+Constraints:
+
+cards.length == 4
+1 <= cards[i] <= 9
+'''
+
+div = lambda x,y: reduce(truediv,(x,y)) if y else inf
+ops = (add, sub, mul, div)
+
+class Solution:
+    def judgePoint24(self, cards: List[int]) -> bool:
+
+        def dfs(c: list) -> bool:
+
+            if len(c) <2 : 
+                return isclose(c[0], 24)
+            
+            for p in set(permutations(c)):
+                for num in {reduce(op,p[:2]) for op in ops}:
+                    if dfs([num] + list(p[2:])): 
+                        return True
+
+            return False        
+        
+        return dfs(cards)
