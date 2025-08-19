a657996eedeb8abe1a02b15687495789e35808ec
Akimi Yano <akimi.mimi.yano@gmail.com>
2025-08-20 05:34:02 +0900

    zeros

---
+++ lc_top150_daily_comp_2024/2348.NumberofZero-FilledSubarrays.py
@@ -0,0 +1,69 @@
+'''
+2348. Number of Zero-Filled Subarrays
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an integer array nums, return the number of subarrays filled with 0.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,0,0,2,0,0,4]
+Output: 6
+Explanation: 
+There are 4 occurrences of [0] as a subarray.
+There are 2 occurrences of [0,0] as a subarray.
+There is no occurrence of a subarray with a size more than 2 filled with 0. Therefore, we return 6.
+Example 2:
+
+Input: nums = [0,0,0,2,0,0]
+Output: 9
+Explanation:
+There are 5 occurrences of [0] as a subarray.
+There are 3 occurrences of [0,0] as a subarray.
+There is 1 occurrence of [0,0,0] as a subarray.
+There is no occurrence of a subarray with a size more than 3 filled with 0. Therefore, we return 9.
+Example 3:
+
+Input: nums = [2,10,2019]
+Output: 0
+Explanation: There is no subarray filled with 0. Therefore, we return 0.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+'''
+
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        left = None
+        total = 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                if left is None:
+                    left = right
+                total += (right-left+1)
+            else:
+                left = None
+        return total
+
+# Another approach:
+
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        cnt, streak = 0, 0
+        for num in nums:
+            if num == 0:
+                streak += 1
+                cnt += streak
+            else:
+                streak = 0
+        return cnt
