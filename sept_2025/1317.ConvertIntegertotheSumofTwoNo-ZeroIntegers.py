652a7d907b7b3ed381cf6042696bc718a26eacdb
Akimi Yano <akimi.mimi.yano@gmail.com>
2025-09-08 12:18:39 -0700

    zeros

--- sept_2025/1317.ConvertIntegertotheSumofTwoNo-ZeroIntegers.py
+++ sept_2025/1317.ConvertIntegertotheSumofTwoNo-ZeroIntegers.py
@@ -51,4 +51,15 @@ class Solution:
                         break
                 else:
                     return [num, the_other]
+
+# Cleaned up code:
+
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        def check(x):
+            return '0' not in str(x)
             
+        for i in range(1, n):
+            j = n - i
+            if check(i) and check(j):
+                return [i, j]
